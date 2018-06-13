275184f07ed77bf6b4240d208887527d9914cc6e
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-06-13 13:27:46 -0400

    Added SecureRandom & envvar based session_secret initialization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
   configure do
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV.fetch('SINATRA_SESSION_SECRET') { SecureRandom.hex(64) }
   end
 
   get '/' do
