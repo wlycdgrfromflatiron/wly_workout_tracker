de55f9a24aa0d05f6ced4d7712acb324a9909bd2
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-06-13 01:20:52 -0400

    deleted development.sqlite in prep for adding *.sqlite to .gitignore, stubbed out User model

--- config/environment.rb
+++ config/environment.rb
@@ -8,4 +8,6 @@ ActiveRecord::Base.establish_connection(
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
+require './app/models/user.rb'
+
 require './app/controllers/application_controller.rb'
