0e2199746ef5f6b9fe96c744c1a42049449b89da
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-06-14 01:33:37 -0400

    Implemented basic new workouts form

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -1,7 +1,17 @@
 class WorkoutController < ApplicationController
     get '/workouts' do
         redirect to '/' unless logged_in?
-        
+
         erb :'/workouts/index'
+    end
+
+    post '/workouts' do
+
+    end
+
+    get '/workouts/new' do
+        redirect to '/' unless logged_in?
+        
+        erb :'/workouts/new'
     end
 end
